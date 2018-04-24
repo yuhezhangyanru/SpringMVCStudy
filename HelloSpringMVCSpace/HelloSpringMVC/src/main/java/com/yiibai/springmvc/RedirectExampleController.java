0a5d4e243a0c18d9189e098ce9f79ad70b738099
yuhezhangyanru <yuhezhangyanru@sina.com>
2018-04-24 09:15:32 +0800

    add test log

--- HelloSpringMVCSpace/HelloSpringMVC/src/main/java/com/yiibai/springmvc/RedirectExampleController.java
+++ HelloSpringMVCSpace/HelloSpringMVC/src/main/java/com/yiibai/springmvc/RedirectExampleController.java
@@ -5,7 +5,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
  
-//functon:用于重定向的控制器，该控制器使得用户在浏览器输入/redirect之后定位到/hello所代表的控制器
+/*
+ * 使用前缀 "redirect:" ，该方法返回字符串，可以重定向到另一页面。参见图：
+ * functon:用于重定向的控制器，该控制器使得用户在浏览器输入/redirect之后定位到/hello所代表的控制器
+ */
 @Controller
 public class RedirectExampleController {
  
--- HelloSpringMVCSpace/HelloSpringMVC/src/main/java/com/yiibai/springmvc/RequestParamExampleController.java
+++ HelloSpringMVCSpace/HelloSpringMVC/src/main/java/com/yiibai/springmvc/RequestParamExampleController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 /**
+ * 使用@RequestParam 注解将请求参数绑定到你的控制器方法参数。
  * @category 该控制器用于在浏览器地址接收一个参数"name"的输入，从而获得不通的处理
  * 例子地址：http://localhost:8080/HelloSpringMVC/user?name=admin
  * 格式相当于/user后加"?name="+参数值 
